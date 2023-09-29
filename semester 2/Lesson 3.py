9984380a63d5cd97d75cb94251a7a48423ac869e
Kapiton <ksardos@mail.ru>
2023-09-29 15:03:43 +0300

    Create Lesson 3.py

---
+++ semester 2/Lesson 3.py
@@ -0,0 +1,21 @@
+# -*- coding: cp1251 -*-
+
+#Задание 1
+kind = input("Введите вид питомца:")
+name = input("Введите кличку питомца:")
+age = input("Введите возраст питомца:")
+
+print ("Это %s по кличке \"%s\". Возраст: %s года."%(kind,name,age))
+
+#Задание 2
+print("Введите все этапы человеческого развития")
+
+stage1=input("Этап 1:")
+stage2=input("Этап 2:")
+stage3=input("Этап 3:")
+stage4=input("Этап 4:")
+stage5=input("Этап 5:")
+stage6=input("Этап 6:")
+
+print("Ваш ответ:")
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=" => ")
