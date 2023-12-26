115da8f4e9a50e21f56f3a160b506bc8f1af8400
Kapiton <ksardos@mail.ru>
2023-12-26 17:23:34 +0300

    Update Lesson_15.py

--- semester 2/Lesson_15.py
+++ semester 2/Lesson_15.py
@@ -19,5 +19,18 @@ autobus = Transport('Renaul Logan', '180', '12')
 print(autobus.info())
 
 # Задание 2
-Autobus = Transport('Renaul Logan', '180', '12')
-print(Autobus.seating_capacity())
+class Autobus(Transport):
+
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+
+    def __str__(self):
+        return "Марка: " + self.name + " Макс. скорость: " + str(self.max_speed) + " Пробег: " + str(
+            self.mileage) + " Вместимость: " + str(self.capacity)
+
+    def seating_capacity(self, capacity=50):
+        return f"Вместимость одного автобуса {self.name} {capacity} пассажиров"
+
+
+autobus = Autobus("Renault Logan", 180, 12)
+print(autobus.seating_capacity())
